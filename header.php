e34b74a651387342d62092c357d7f21dfe9c5b79
tboulund <tbmh@easv.dk>
2024-08-30 08:52:55 +0200

    Added wp_head()-call

--- header.php
+++ header.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?php bloginfo("name") ?></title>
+    <?php wp_head() ?>
 </head>
 <body>
     <header>
